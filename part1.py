c541edcb1fe252c9473724227391e5d46602ac07
CodeDragon82 <michaelrichar280@outlook.com>
2023-05-06 18:49:56 +0100

    Implemented function to read test data and write the predicted labels to output results.

--- part1.py
+++ part1.py
@@ -2,43 +2,55 @@ import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import *
 from sklearn.model_selection import train_test_split
 
 from sklearn.svm import LinearSVC
 
+TRAINING_DATA_FILE = 'TrainingDataBinary.csv'
+TESTING_DATA_FILE = 'TestingDataBinary.csv'
+RESULTS_DATA_FILE = 'TestingResultsBinary.csv'
+
 scaler = StandardScaler()
 
-def train(model, data):
+def train(model):
+    data = pd.read_csv(TRAINING_DATA_FILE, header=None)
+
+    # Seperate the input 'features' from the expect output labels.
     X = data.iloc[:, :-1].values
     y = data.iloc[:, -1].values
 
     X = scaler.fit_transform(X)
 
-    model.fit(X, y)
+    # Seperate the training data into 2 parts.
+    X_1, X_2, y_1, y_2 = train_test_split(X, y, train_size=0.7)
 
-def test(model, data):
-    X = data.iloc[:, :-1].values
-    y_expected = data.iloc[:, -1].values
+    # Use the first part of the training data to train the model.
+    model.fit(X_1, y_1)
 
+    # Use the second part of the training data to validate the accuracy of the model.
+    y_predicted = model.predict(X_2)
+    print('Accuracy: ', accuracy_score(y_2, y_predicted))
+    print('Precision: ', precision_score(y_2, y_predicted))
+    print('Recall: ', recall_score(y_2, y_predicted))
+
+def test(model):
+    data = pd.read_csv(TESTING_DATA_FILE, header=None)
+
+    X = data.values
     X = scaler.fit_transform(X)
 
-    y_actual = model.predict(X)
+    y = model.predict(X)
 
-    accuracy = accuracy_score(y_expected, y_actual)
-    print('Accuracy: ', accuracy)
+    data['labels'] = y
 
-# Load in dataset.
-data = pd.read_csv('TrainingDataBinary.csv')
+    data.to_csv(RESULTS_DATA_FILE, header=False, index=False)
 
-# Seperate dataset into training and testing data.
-training_data, testing_data = train_test_split(data, train_size=0.7)
-
-# Instantiate linear SVC model.
+# Instantiate model.
 model = LinearSVC()
 
 # Train model with training data.
-train(model, training_data)
+train(model)
 
-# Test model with testing data.
-test(model, testing_data)
+# Test model with validating data.
+test(model)
